0ad2586ec3173d60c2ce049b6b6972ef750cb48a
yuki1567 <19961017yuki@gmail.com>
2024-08-03 03:32:48 +0900

    refactor: 起動コマンドを分かりやすいものに変更

--- src/start_application.sh
+++ src/start_application.sh
@@ -9,8 +9,8 @@ echo "MySQL server is available!"
 cd /var/www/html/src/
 npm install
 npm install -g pm2
-pm2 start "npm run dev" --name nuxt
-pm2 start "npm run start" --name api
+pm2 start "npm run nuxt-dev" --name nuxt
+pm2 start "npm run api-dev" --name api
 
 # Nginxの起動
 service nginx start
