e92fae063a26fd5f0878f2c4ae1f03a97468bd47
yuki1567 <19961017yuki@gmail.com>
2024-08-03 03:31:52 +0900

    chore: リセットCSS導入

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,7 +1,12 @@
 import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // ssr無効化
   ssr: false,
 
+  // 参照するフロントのディレクトリ設定
   srcDir: 'front',
+
+  // Global CSS リセットCSS
+  css: ['ress'],
 })
