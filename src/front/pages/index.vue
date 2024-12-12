bb910350e0a202e62821b7f29080dd0c1c0b4e74
yuki1567 <19961017yuki@gmail.com>
2024-12-12 22:08:40 +0900

    add: ログイン画面作成

--- src/front/pages/index.vue
+++ src/front/pages/index.vue
@@ -1,6 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BookList from '../template/BookList.vue'
+import Header from '../template/Header.vue'
+import Footer from '../template/Footer.vue'
+import Login from '../template/Login.vue'
+import Sample from '../template/sample.vue'
+
+const isLogin = false
+</script>
 <template>
-  <h1>HELLO</h1>
-  <Book />
+  <div id="app">
+    <Login />
+    <template v-if="isLogin">
+      <Header />
+      <BookList />
+      <Footer />
+    </template>
+    <!-- <Sample /> -->
+  </div>
 </template>
-<style></style>
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  background-color: #f7f9fc;
+  height: 100vh;
+  color: #333;
+}
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/front/template/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <main>
+      <div class="main-body">
+        <img src="../assets/images/logo.svg" alt="ロゴ" class="logo-img" />
+        <form>
+          <input type="text" placeholder="メールアドレス" class="text-field" />
+          <input type="password" placeholder="パスワード" class="text-field" />
+          <button type="submit" class="submit-button">ログイン</button>
+        </form>
+      </div>
+    </main>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 700px;
+  margin: auto;
+  height: 100vh;
+}
+
+main {
+  border-radius: 5px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+
+  .logo-img {
+    display: block;
+    width: 60px;
+  }
+
+  .main-body {
+    background-color: white;
+    padding: 50px 0px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    form {
+      display: flex;
+      flex-direction: column;
+      width: 400px;
+
+      .text-field {
+        padding: 10px;
+        margin: 20px;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+      }
+
+      .submit-button {
+        background-color: rgb(0, 170, 0);
+        border-radius: 30px;
+        padding: 15px;
+        margin: 30px 20px;
+        width: 200px;
+        display: block;
+        margin: 10px auto;
+      }
+    }
+  }
+}
+</style>
