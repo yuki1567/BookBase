d1a52797d669006192d559c61448818e957fef7b
yuki1567 <19961017yuki@gmail.com>
2024-12-15 03:41:03 +0900

    add: 警告が出ていたため、オプションの設定を追加

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -9,4 +9,12 @@ export default defineNuxtConfig({
 
   // Global CSS リセットCSS
   css: ['ress'],
+
+  vite: {
+    define: {
+      __VUE_OPTIONS_API__: false, // Options APIを有効化
+      __VUE_PROD_DEVTOOLS__: false, // 本番環境でDevTools無効
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // 本番環境でHydrationエラーログ無効
+    },
+  },
 })
