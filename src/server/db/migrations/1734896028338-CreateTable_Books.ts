34ff68a48dd61ac80ab90233ae2c18e47fd71474
yuki1567 <19961017yuki@gmail.com>
2024-12-23 04:41:35 +0900

    add: マイグレーションファイルの生成

---
+++ src/server/db/migrations/1734896028338-CreateTable_Books.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateTableBooks1734896028338 implements MigrationInterface {
+  name = 'CreateTableBooks1734896028338'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`books\` (\`id\` int UNSIGNED NOT NULL AUTO_INCREMENT, \`title\` varchar(255) NOT NULL, \`description\` text NULL, \`price\` int UNSIGNED NOT NULL, \`status\` int UNSIGNED NOT NULL, \`created_at\` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP, \`updated_at\` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`books\``)
+  }
+}
