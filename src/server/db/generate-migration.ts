c49a5641d3de901274923fa1d3282fcb802485e6
yuki1567 <19961017yuki@gmail.com>
2024-12-22 23:33:36 +0900

    refactor: 関数化し、可読性の向上

--- src/server/db/generate-migration.ts
+++ src/server/db/generate-migration.ts
@@ -2,59 +2,85 @@ import { execSync } from 'child_process'
 import { resolve } from 'path'
 import { writeFileSync, existsSync, unlinkSync } from 'fs'
 
-const entityName = process.argv[2]
-const operation = process.argv[3]
-if (!entityName || !operation) {
-  console.error(
-    '[ERROR] エンティティ名とマイグレーションの種類を引数で指定してください: 例）ts-node generate-migration.ts User CreateTable',
-  )
-  process.exit(1)
+function main(): void {
+  const { entityName, operation } = getCommandLineArguments()
+  const tempDataSourcePath = createTempDataSourceFile(entityName)
+  generateMigration(operation, entityName, tempDataSourcePath)
+  removeTempDataSourceFile(tempDataSourcePath)
 }
 
-const tempDataSourcePath = resolve(
-  __dirname,
-  `temp-data-source-${entityName}.ts`,
-)
-const entityPath = resolve(__dirname, `entities/${entityName}`)
+function getCommandLineArguments(): { entityName: string; operation: string } {
+  const entityName = process.argv[2]
+  const operation = process.argv[3]
 
-// エンティティごとにマイグレーションするため個別のDB接続情報を設定
-const dataSourceContent = `
-import { DataSource } from 'typeorm'; 
-import { ${entityName} } from '${entityPath}'
+  if (!entityName || !operation) {
+    console.error(
+      '[ERROR] エンティティ名とマイグレーションの種類を引数で指定してください: 例）ts-node generate-migration.ts User CreateTable',
+    )
+    process.exit(1)
+  }
 
-const dataSource = new DataSource({
-  type: 'mysql',
-  host: 'mysql',
-  port: 3306,
-  username: 'root',
-  password: 'password',
-  database: 'BookBase',
-  synchronize: false,
-  logging: false,
-  entities: [${entityName}],
-  migrations: [],
-  subscribers: [],
-  migrationsRun: false,
-})
+  return { entityName, operation }
+}
 
-export default dataSource
-`
+function createTempDataSourceFile(entityFileName: string): string {
+  const tempDataSourcePath = resolve(
+    __dirname,
+    `temp-data-source-${entityFileName}.ts`,
+  )
+  const entityPath = resolve(__dirname, `entities/${entityFileName}`)
 
-writeFileSync(tempDataSourcePath, dataSourceContent)
+  // エンティティごとにマイグレーションするため個別のDB接続情報を設定
+  const dataSourceContent = `
+    import { DataSource } from 'typeorm'; 
+    import { ${entityFileName} } from '${entityPath}'
 
-const migrationDir = resolve(
-  __dirname,
-  `./migrations/${operation}_${entityName}s`,
-)
-const migrationCommand = `npx typeorm-ts-node-commonjs migration:generate ${migrationDir} -d ${tempDataSourcePath}`
+    const dataSource = new DataSource({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'BookBase',
+      synchronize: false,
+      logging: false,
+      entities: [${entityFileName}],
+      migrations: [],
+      subscribers: [],
+      migrationsRun: false,
+    })
 
-try {
-  execSync(migrationCommand, { stdio: 'pipe' })
-  console.log('[SUCCESS] マイグレーション完了')
-} catch (error) {
-  console.error(`[ERROR] ${error.message}`)
-} finally {
-  if (existsSync(tempDataSourcePath)) {
-    unlinkSync(tempDataSourcePath)
+    export default dataSource
+  `
+
+  writeFileSync(tempDataSourcePath, dataSourceContent)
+
+  return tempDataSourcePath
+}
+
+function generateMigration(
+  migrationAction: string,
+  targetEntity: string,
+  configurationFilePath: string,
+): void {
+  const migrationPath = resolve(
+    __dirname,
+    `./migrations/${migrationAction}_${targetEntity}s`,
+  )
+  const migrationCommand = `npx typeorm-ts-node-commonjs migration:generate ${migrationPath} -d ${configurationFilePath}`
+
+  try {
+    execSync(migrationCommand, { stdio: 'pipe' })
+    console.log('[SUCCESS] マイグレーション完了')
+  } catch (error) {
+    console.error(`[ERROR] ${error.message}`)
   }
 }
+
+function removeTempDataSourceFile(removeFilePath): void {
+  if (existsSync(removeFilePath)) {
+    unlinkSync(removeFilePath)
+  }
+}
+
+main()
