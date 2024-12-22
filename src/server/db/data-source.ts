13b48d6afbd157fb12e3e57c94b10fc47238b079
yuki1567 <19961017yuki@gmail.com>
2024-12-22 18:19:17 +0900

    fix: TypeORMでエンティティをもとにDB情報を作成するように修正

--- src/server/db/data-source.ts
+++ src/server/db/data-source.ts
@@ -1,26 +1,26 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entities/User"
-const entities = [
-  User,
-]
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import path from 'path'
 
-export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: "mysql",
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'mysql',
   port: 3306,
-  username: "root",
-  password: "password",
-  database: "BookBase",
+  username: 'root',
+  password: 'password',
+  database: 'BookBase',
   synchronize: false,
-  logging: false,
-  entities: entities,
-  migrations: [],
+  logging: true,
+  entities: [path.join(__dirname, './entities/*.ts')],
+  migrations: [path.join(__dirname, './migrations/*.ts')],
   subscribers: [],
+  migrationsRun: false,
 })
+
+export default AppDataSource
 
 AppDataSource.initialize()
   .then(() => {
-    console.log("Inserting a new user into the database...")
+    console.log('Inserting a new user into the database...')
   })
   .catch((error) => console.log(error))
--- src/server/db/entities/Book.ts
+++ src/server/db/entities/Book.ts
@@ -1,21 +1,24 @@
 import {
-  Entity,
-  PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm'
 
-@Entity('users')
-export class User {
+@Entity('books')
+export class Book {
   @PrimaryGeneratedColumn({ type: 'int', unsigned: true })
-  readonly id: number
+  id: number
 
-  @Column('varchar', { length: 255, unique: true, nullable: false })
-  email: string
+  @Column('varchar', { length: 255, nullable: false })
+  title: string
 
-  @Column('varchar', { length: 50, nullable: false })
-  password: string
+  @Column('text', { nullable: true })
+  description: string | null
+
+  @Column('int', { unsigned: true, nullable: false })
+  price: number
 
   @Column('int', { unsigned: true, nullable: false })
   status: number
