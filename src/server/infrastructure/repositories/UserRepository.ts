f62d4abda93d6a5bfbace830a8091c2ab01d1f99
yuki1567 <19961017yuki@gmail.com>
2025-01-18 22:41:54 +0900

    fix: DBから取得した値をドメイン層で管理できるように修正

--- src/server/infrastructure/repositories/UserRepository.ts
+++ src/server/infrastructure/repositories/UserRepository.ts
@@ -1,13 +1,18 @@
 import { IUserRepository } from '@/application/repositories/IUserRepository'
 import { User } from '@/domain/User'
 import { Database } from '@/infrastructure/database/Database'
+import { UserORM } from '@/infrastructure/database/entities/UserORM'
 
 export class UserRepository implements IUserRepository {
   public async findByEmail(email: string): Promise<User | undefined> {
     const dbConnect = Database.getDbConnect()
-    const userRepository = dbConnect.getRepository(User)
-    const user = await userRepository.findOneBy({ email })
+    const userRepository = dbConnect.getRepository(UserORM)
+    const userEntity = await userRepository.findOneBy({ email })
 
-    return user || undefined
+    const user = userEntity
+      ? new User(userEntity.id, userEntity.email, userEntity.password)
+      : undefined
+
+    return user
   }
 }
