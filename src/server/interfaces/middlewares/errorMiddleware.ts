df1f237ec3fc15648d972be9b6b20dec9b8f1af8
yuki1567 <19961017yuki@gmail.com>
2025-01-25 02:12:51 +0900

    refactor: エラーハンドハンドリングを関数を細かく分け、わかりやすく修正

--- src/server/interfaces/middlewares/errorMiddleware.ts
+++ src/server/interfaces/middlewares/errorMiddleware.ts
@@ -9,34 +9,43 @@ export function errorMiddleware(
   _next: NextFunction,
 ): void {
   if (error instanceof ApplicationError) {
-    console.error(error)
-    res.status(error.errorResponse.statusCode).json(error.errorResponse)
+    handleApplicationError(res, error)
   } else if (error instanceof z.ZodError) {
-    console.error(error)
-    const details = error.errors.map((e) => {
-      console.log(e)
-      const [field] = e.path
-
-      return {
-        title: 'バリデーションエラー',
-        field: String(field),
-        message: e.message,
-      }
-    })
-
-    const errorResponse = new ApplicationError({
-      isSuccess: false,
-      statusCode: 200,
-      details: details,
-    })
-    res
-      .status(errorResponse.errorResponse.statusCode)
-      .json(errorResponse.errorResponse)
+    handleZodError(res, error)
   } else {
-    console.error(error)
-    const errorResponse = ApplicationError.formatErrorCode('UNKNOWN_ERROR')
-    res
-      .status(errorResponse.errorResponse.statusCode)
-      .json(errorResponse.errorResponse)
+    handleUnkownError(res, error)
   }
+}
+
+function handleApplicationError(response: Response, error: ApplicationError) {
+  console.error(error)
+  response.status(error.errorResponse.statusCode).json(error.errorResponse)
+}
+
+function handleZodError(response: Response, error: z.ZodError) {
+  console.error(error)
+  const details = error.errors.map(({ path, message }) => {
+    return {
+      title: 'バリデーションエラー',
+      field: String(path),
+      message: message,
+    }
+  })
+
+  const errorResponse = new ApplicationError({
+    isSuccess: false,
+    statusCode: 200,
+    details: details,
+  })
+  response
+    .status(errorResponse.errorResponse.statusCode)
+    .json(errorResponse.errorResponse)
+}
+
+function handleUnkownError(response: Response, error: unknown) {
+  console.error(error)
+  const errorResponse = ApplicationError.formatErrorCode('UNKNOWN_ERROR')
+  response
+    .status(errorResponse.errorResponse.statusCode)
+    .json(errorResponse.errorResponse)
 }
